c9b3976f7ead451a84ad54a743123c3c910fa11a
Your Name <ravalokimberly@gmail.com>
2025-01-09 18:01:58 +0800

    Create-student functionality

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { StudentController } from './student.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentService } from './student.service';
+import { StudentController } from './student.controller';
+import { Student } from './student.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Student])], // Register the Student entity
   controllers: [StudentController],
-  providers: [StudentService]
+  providers: [StudentService],
+  exports: [StudentService], // Export if used in other modules
 })
 export class StudentModule {}
