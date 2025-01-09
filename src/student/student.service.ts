5e305966dbfa5a96d0c4ef3585d0ed36a325ae2e
Your Name <ravalokimberly@gmail.com>
2025-01-09 17:54:36 +0800

    Create Student feature

---
+++ src/student/student.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(student: Partial<Student>): Promise<Student> {
+    return this.studentRepository.save(student);
+  }
+}
